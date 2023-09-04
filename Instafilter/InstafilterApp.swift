8268d000472ce43b3ec00de66d4a80a78f36e113
Artyom Bashkatov <ar.bashkatov@saychas.ru>
2023-09-04 17:45:10 +0300

    Added Project to github

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Артём Башкатов on 06.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
